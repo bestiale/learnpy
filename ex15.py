12bb4127f1b9c24003a016cf1984a8e0c758c28d
rickyfh <rc@feinheit.ch>
2012-10-10 12:09:20 +0200

    exercise 15

--- ex15.py
+++ ex15.py
@@ -7,8 +7,7 @@ txt = open(filename)
 print "Here's your file %r:" % filename
 print txt.read()
 
-print "Type the filename again:"
-file_again = raw_input("> ")
+file_again = raw_input("> Type the filename again:")
 
 txt_again = open(file_again)
 
