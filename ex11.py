1c77221547d1ca7b76a2981b086fb55ff69f8d1d
rickyfh <rc@feinheit.ch>
2012-10-10 10:37:13 +0200

    on the end a little bit maths

--- ex11.py
+++ ex11.py
@@ -6,3 +6,13 @@ print "How much do you weight?",
 weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "\nI give you the Result of two added numbers\n"
+print "Give me a number: \n",
+first = int(raw_input())
+print "Give me a second number: \n",
+second = int(raw_input())
+
+result = first + second
+
+print "%d + %d = %d" % (first, second, result)
