2ccd987c4c499ccfe0e56514fb234ee11a6ac202
rickyfh <rc@feinheit.ch>
2012-10-15 13:56:54 +0200

    exercise 33

--- ex33.py
+++ ex33.py
@@ -1,7 +1,13 @@
+def print_numbers(numbers):
+    print "The numbers: "
+    for num in numbers:
+        print num
+    
 i = 0
+max_count = 6
 numbers = []
 
-while i < 6:
+while i < max_count:
     print "At the top i is %d" % i
     numbers.append(i)
     
@@ -9,7 +15,5 @@ while i < 6:
     print "Numbers now: ", numbers
     print "At the bottom i is %d" % i
 
-print "The numbers: "
+print_numbers(numbers)
 
-for num in numbers:
-    print num
