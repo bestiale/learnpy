4dda3b6c6047f5a79cf82e6888d098955b9cb2c6
rickyfh <rc@feinheit.ch>
2012-10-15 14:41:42 +0200

    exercise 34

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+
+print "The animal at 1 is the 2nd and is a %s" % animals[1]
+print "The 3rd animal is at 2 and is a %s" % animals[2]
+print "The 1st animal ist at 0 and is a %s" % animals[0]
+print "The animal at 3 is the 4th and is a %s" % animals[3]
+print "The 5th animal is at 4 and is a %s" % animals[4]
+print "The animal at 2 is the 3rd and is a %s" % animals[2]
+print "The 6th animal is at 5 and is a %s" % animals[5]
+print "The animal at 4 is the 5th and is a %s" % animals[4]
