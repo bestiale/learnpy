29cb8365f1f0ddcc1ff545dc9f7fdc1a0d56f7f1
rickyfh <rc@feinheit.ch>
2012-10-15 14:01:57 +0200

    exercise 33

--- ex33.py
+++ ex33.py
@@ -2,18 +2,19 @@ def print_numbers(numbers):
     print "The numbers: "
     for num in numbers:
         print num
-    
+
+def count(i, max_count):
+    while i < max_count:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+        
 i = 0
 max_count = 6
 numbers = []
-
-while i < max_count:
-    print "At the top i is %d" % i
-    numbers.append(i)
-    
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
-
+        
+count(i, max_count)
 print_numbers(numbers)
-
