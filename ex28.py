44df5beedd5517ddbf82f8297c5ca49177a52166
rickyfh <rc@feinheit.ch>
2012-10-15 11:43:45 +0200

    exercise 28

---
+++ ex28.py
@@ -0,0 +1,20 @@
+print True and True # True
+print False and True # False
+print 1 == 1 and 2 == 1 # False
+print "test" == "test" # True
+print 1 == 1 or 2 != 1 # True
+print True and 1 == 1 # True
+print False and 0 != 0 # False
+print True or 1 == 1 # True
+print "test" == "testing" # False
+print 1 != 0 and 2 == 1 # False 
+print "test" != "testing" # True
+print "test" == 1 # False
+print not (True and False) # True
+print (1 == 1 and 0 != 1) # True
+print (10 == 1 or 1000 == 1000) # True
+print (1 != 10 or 3 == 4) # True
+print ("testing" == "testing" and "Zed" == "Cool Guy") # False
+print 1 == 1 and not ("testing" == 1 or 1 == 0) # True
+print "chunky" == "bacon" and not (3 == 4 or 3 == 3) # False
+print 3 == 3 and not ("testing" == "testing" or "Python" == "Fun") # False
