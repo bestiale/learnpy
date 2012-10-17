4584b5e0f6b79ecf3b5ea772f0877d6ee4f4abc8
rickyfh <rc@feinheit.ch>
2012-10-17 10:26:21 +0200

    ecercise 44

--- ex44.py
+++ ex44.py
@@ -1,9 +1,18 @@
 class Parent(object):
 
+	def override(self):
+		print "PARENT override()"
+
+	def implicit(self):
+		print "PARENT implicit()"
+
 	def altered(self):
 		print "PARENT altered()"
 
 class Child(Parent):
+
+	def override(self):
+		print "CHILD override()"
 
 	def altered(self):
 		print "CHILD,BEFORE PARENT altered()"
@@ -12,6 +21,12 @@ class Child(Parent):
 
 dad = Parent()
 son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
 
 dad.altered()
 son.altered()
